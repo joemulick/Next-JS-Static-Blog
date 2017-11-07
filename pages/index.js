a641dc91703eb317989276b38fb6ea569e8b71a6
Joe Mulick <joemulick@gmail.com>
2017-11-06 20:29:48 -0800

    updated package.json

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Page from '../src/components/Page'
 import PagePreview from '../src/components/PagePreview'
 import { formatDate } from '../src/utils/date'
 import { makeUrl, filterPosts } from '../src/utils/content'
-import { Alert } from 'reactstrap'
 import CONFIG from '../content/index.json'
 import SUMMARY_JSON from '../content/summary.json'
 
@@ -26,9 +25,6 @@ function Body(props) {
 
       <h1>Test from inside pages index.js</h1>
 
-      <Alert color="primary">
-        This is a primary alert — check it out!
-      </Alert>
     </div>
   )
 }
